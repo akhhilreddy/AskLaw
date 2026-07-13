281388f2271e1dce7d0f44434a092f0dd66855dd
akhhilreddy <akhilreddypalamakula777@gmail.com>
2026-07-13 11:58:53 +0530

    Implement secure user registration with MongoDB

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,13 +1,32 @@
 from fastapi import APIRouter
+from app.db.mongodb import user_collection
+from app.utils.security import hash_password
+from fastapi import HTTPException, status
 
 from app.schemas.auth import SignUpRequest
 
 router = APIRouter()
 
-@router.post("/signup")
+@router.post("/signup",
+             status_code=status.HTTP_201_CREATED)
 def signup(user : SignUpRequest):
-    return {
+    existing_user = user_collection.find_one({"email" : user.email})
+    if existing_user:
+        raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail="Email already exists"
+    )
+    hashed_password = hash_password(user.password)
+
+    user_document = {
         "name" : user.name,
         "email" : user.email,
-        "message" : "SignUp endpoint reached successfully"
+        "password_hash" : hashed_password
     }
+
+    user_collection.insert_one(user_document)
+
+    return{
+        "message" : "User registered successfully"
+    }
+    
